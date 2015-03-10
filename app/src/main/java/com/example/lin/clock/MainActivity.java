a2aff51ff4308c7ee2456acaa425b39c29942b28
Lin <jassy.xiao@gmail.com>
2015-03-10 16:33:32 -0400

    test

--- app/src/main/java/com/example/lin/clock/MainActivity.java
+++ app/src/main/java/com/example/lin/clock/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.lin.clock;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    public TextView tv;
+    public Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.g);
+        b = (Button)findViewById(R.id.b);
+
     }
 
+    public void selfDestruct(View view) {
+        CountDownTimer ct = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tv.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
 
+            @Override
+            public void onFinish() {
+                tv.setText("done");
+            }
+        };
+        ct.start();
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
